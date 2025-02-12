89a1c637ee930a4c8f6972f318ada15c312dbbea
liScholar <liScholar@git.cn>
2025-02-12 12:31:14 +0800

    second commit

---
+++ smallprogram/app01/llm/chatai.py
@@ -0,0 +1,49 @@
+from app01.models import ChatSessionMessage
+import uuid
+import json
+
+
+# 千问的ai接口
+class OpenaiQianwenapi:
+    def __init__(self, request):
+        self.request = request
+    
+
+
+
+
+
+
+
+
+
+
+
+
+# 有关历史记录的获取
+class ChatAiInformation:
+    def __init__(self, request):
+        self.request = request
+
+    # 获取session_id
+    def acquire_session_id(self):
+        data = json.loads(self.request.body)
+        session_id = data.get('session_id')
+        if not session_id:
+            session_id = uuid.uuid4().hex
+        return session_id
+    
+    def acquire_history_message(self, session_id):
+        history_message = []
+
+        data_exist = ChatSessionMessage.objects.filter(session_id=session_id).exists()
+        if not data_exist:
+            return history_message
+        
+        row_obj_history = ChatSessionMessage.objects.filter(session_id=session_id).order_by('create_time')
+
+        for msg in row_obj_history:
+
+        
+
+    
--- smallprogram/app01/models.py
+++ smallprogram/app01/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,32 @@ class UserInfo(models.Model):
     password = models.CharField(max_length=64)
 
 
+"""
+    {
+        用户名(
+            {
+                类型:
+                数据:
+            }
+            {
+                类型:
+                数据:
+            }
+
+        ai(
+            {
+                类型:
+                数据:
+            }
+        )
+    }
+"""
+
+
+class ChatSessionMessage(models.Model):
+    session_id = models.UUIDField()
+    message = models.TextField()
+    role = models.CharField(max_length=16)
+    create_time = models.DateTimeField(auto_now_add=True)
+    type_message = models.CharField(max_length=16, default='text')
+    
